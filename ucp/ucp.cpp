d53c92cdcf57603a7e36ff0ca23faf2844d4bf35
agilimam <agilimamalhafizi04@gmail.com>
2023-04-07 10:36:00 +0700

    membuat int Agil

--- ucp/ucp.cpp
+++ ucp/ucp.cpp
@@ -1,3 +1,10 @@
+// 1. Karena suatu masalah yang memiliki banyak data akan lebih mudah diselesaikan melalui algorithma
+// 2. Prosedural dan objek
+// 3. Metode, program ,
+// 4. Quick Sort karena algoritma ini sangat cepat dan efisien , algoritma ini membagi array menjadi dua bagian , satu bagian elemen yang lebih kecil dan bagian satunya memuat elemen yang lebih besar dari pivot.Kemudian , algoritma ini dijalankan secara rekursif pada kedua bagian array tersebut hingga seluruh array terurut.
+// 5. bubbleSort, insertionSort dan SelectionSort untuk linequer MergeSort
+// 6.
+
 #include <iostream>
 using namespace std;
 
@@ -5,3 +12,7 @@ const int MAX_DATA = 125; // maximum number of data
 const string INITIALS = "AI"; // change "j" to the two initials of your name
 const string NICKNAME = "Agil"; // change "arr" with your nickname
 
+// Procedure Input to get user input for the array
+void input(int Agil[], int& n) {
+    cout << "Masukkan jumlah data (maksimum " << MAX_DATA << "): ";
+    cin >> n;
