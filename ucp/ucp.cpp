87b852a32e20259e8a3c98185090c7265d3359c5
agilimam <agilimamalhafizi04@gmail.com>
2023-04-07 10:31:39 +0700

    Add project files.

---
+++ ucp/ucp.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_DATA = 125; // maximum number of data
+const string INITIALS = "AI"; // change "j" to the two initials of your name
+const string NICKNAME = "Agil"; // change "arr" with your nickname
