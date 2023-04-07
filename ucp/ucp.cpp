82cac184ea25cdd8e49bdea0482b8f019818b644
agilimam <agilimamalhafizi04@gmail.com>
2023-04-07 10:32:13 +0700

    membuat maximun data

--- ucp/ucp.cpp
+++ ucp/ucp.cpp
@@ -4,3 +4,4 @@ using namespace std;
 const int MAX_DATA = 125; // maximum number of data
 const string INITIALS = "AI"; // change "j" to the two initials of your name
 const string NICKNAME = "Agil"; // change "arr" with your nickname
+
